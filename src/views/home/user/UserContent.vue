00fe8453e3791d005991dbd1a55e2ee3eb0d30f4
tah9 <690818882@qq.com>
2021-09-30 16:31:56 +0800

    个人页面修改

---
+++ src/views/home/user/UserContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content-view">
+    <div class="text-div">他的热门动态 <van-icon name="arrow" /></div>
+    <RecommendArticle></RecommendArticle>
+  </div>
+</template>
+
+<script>
+import RecommendArticle from "@/views/home/home/compone/RecommendArticle";
+
+export default {
+  name: "UserContent",
+  components:{
+    RecommendArticle
+  }
+}
+</script>
+
+<style scoped lang="less">
+.content-view {
+}
+.text-div {
+  padding: @item-margin;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
