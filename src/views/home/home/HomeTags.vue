5489cbcd599a3d6db6e234371ae7e81b55cc3723
HaoGre <1730823504@qq.com>
2021-09-30 11:41:00 +0800

    test

--- src/views/home/home/HomeTags.vue
+++ src/views/home/home/HomeTags.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-
+    <p>Hello</p>
   </div>
 </template>
 
 <script>
 
+import Probe from "../../Probe/Probe";
 export default {
   name: "HomeTags",
+  components: {Probe},
   created() {
 
   }
